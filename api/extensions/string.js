c7e42b509ae247aa8379c1dfe7c8bc2179fe8048
Artyom Antyipin <artyom.szasa@gmail.com>
2017-08-14 18:46:03 +0200

    String extensions: toBase64String and fromBase64String.

--- api/extensions/string.js
+++ api/extensions/string.js
@@ -18,5 +18,34 @@ dope.initComponent({
                 return a === b ? 0 : (a < b ? -1 : 1);
             };
         }
+        const toSolidByte = (_, num) => String.fromCharCode('0x' + num);
+        /**
+         * Encodes string as UTF-8 encoded base64 string.
+         *
+         * @method toBase64String
+         * @memberof String
+         * @instance
+         * @return {String} UTF-8 encoded base64 string.
+         */
+        String.prototype.toBase64String = function () {
+            const fixed = encodeURIComponent(this).replace(/%([0-9A-F]{2})/g, toSolidByte);
+            return window.btoa(fixed);
+        };
+        /**
+         * Decodes UTF-8 encoded base64 string.
+         *
+         * @method fromBase64String
+         * @memberof String
+         * @static
+         * @param {String} encoded - UTF-8 encoded base64 string.
+         * @return Decoded string.
+         */
+        String.fromBase64String = encoded => {
+            const fixed = window.atob(encoded)
+                .split('')
+                .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+                .join('');
+            return decodeURIComponent(fixed);
+        };
     }
 });
